36a29ebff527eb595d83bbacc97bc4a4e9d2baeb
f4xdev <100133671+f4xdev@users.noreply.github.com>
2022-09-25 00:05:53 -0300

    first-commit
    .

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded' ,() => {
+    const elementosCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosCarousel,{
+        duration: 300,
+         dist:-80,
+         shift:15,
+         padding:5 , 
+         numVisible:3,
+         indicators:true,
+         noWrap:false
+    });
+});
